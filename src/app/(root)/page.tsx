c2bc9eec8153b988bb5b0bc8bed5852553a0eb4d
Edgar Fajardo <edgarcfajardo@gmail.com>
2025-10-23 13:04:10 -0600

    feat: navbar

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Navbar } from "./components";
+
+export default function Home() {
+  return (
+    <div 
+      className="min-h-screen w-full bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
+      < Navbar />
+    </div>
+  );
+}
