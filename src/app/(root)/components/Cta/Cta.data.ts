af65687f999fe5ca9987ce2875f843b1cf5983c9
Edgar Fajardo <edgarcfajardo@gmail.com>
2025-10-26 22:28:30 -0600

    feat: cta home

---
+++ src/app/(root)/components/Cta/Cta.data.ts
@@ -0,0 +1,14 @@
+export const ctaData = [
+    {
+        title: "10k+",
+        description: "Developers Trained",
+    },
+    {
+        title: "94%+",
+        description: "Success Rate",
+    },
+    {
+        title: "500+",
+        description: "Companies Hiring",
+    }
+]
